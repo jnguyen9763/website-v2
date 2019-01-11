f958b0eb38121e57a8c02903d0d0d07bf692ccc2
Jacqueline Nguyen <jnguyen9763@gmail.com>
2019-01-11 16:27:14 -0500

    Added turning side to side to petals

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,12 +1,13 @@
-var img, numPetals;
+var img, numPetals, heightFloor;
 var petals = [];
 
 numPetals = 50;
+heightFloor = 10;
 
 function setup() {
     angleMode(DEGREES);
     imageMode(CENTER);
-    createCanvas($(window).width(), $(window).height() - 20);
+    var canvas = createCanvas(document.body.clientWidth, document.body.clientHeight);
     img = loadImage('../images/petal.png');
     for (var i = 0; i < numPetals; i++) {
         petals[i] = new Petal();
@@ -28,21 +29,20 @@ function draw() {
 }
 
 function mouseClicked() {
-    console.log(mouseX, mouseY);
     for (var i = 0; i < petals.length; i++) {
         if ((mouseX < petals[i].posX + 5 && mouseX > petals[i].posX - 5)  && (mouseY < petals[i].posY + 5 && mouseY > petals[i].posY - 5)) {
-            console.log('Yes!');
             petals[i].falling = false;
         }
     }
 }
 
 function windowResized() {
-  resizeCanvas($(window).width(), $(window).height() - 20);
+  resizeCanvas(document.body.clientWidth, document.body.clientHeight);
 }
 
 function Petal() {
-    this.rotation = random(360);
+    this.OGrotation = random(360);
+    this.rotation = this.OGrotation;
     this.lenX = random(15, 20);
     this.lenY = this.lenX / 2;
     this.posX = random(width);
@@ -50,12 +50,16 @@ function Petal() {
     this.speedX = random(-2, 2);
     this.speedY = random(1, 2);
     this.falling = true;
+    this.turn_left = random([true, false]);
+    this.rotate = true;
+    this.change = false;
 
+    // NEED TO REDO THIS
     this.jump = function() {
         this.speedY = random(1, 2);
         this.posY -= this.speedY;
 
-        if (this.posY < height - 50) {
+        if (this.posY < height - heightFloor) {
             this.falling = true;
         }
 
@@ -71,9 +75,10 @@ function Petal() {
         this.posX += this.speedX;
         this.posY += this.speedY;
 
-        if (this.posY > height - 5) {
+        if (this.posY > height - heightFloor) {
             this.speedX = 0;
             this.speedY = 0;
+            this.rotate = false;
         }
 
         if (this.posX < 5) {
@@ -90,5 +95,29 @@ function Petal() {
         rotate(this.rotation);
         image(img, 0, 0, this.lenX, this.lenY);
         pop();
+
+        if (this.rotate) {
+            if (abs(this.OGrotation - this.rotation) < 15 || this.change) {
+                if (this.change) {
+                    if (this.turn) {
+                        this.turn = false;
+                    }
+                    else {
+                        this.turn = true;
+                    }
+                    this.change = false;
+                }
+
+                if (this.turn) {
+                    this.rotation += random(0, 3);
+                }
+                else {
+                    this.rotation -= random(0, 3);
+                }
+            }
+            else {
+                this.change = true;
+            }
+        }
     }
 }
