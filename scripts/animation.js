bb31307aec5ee82d8ffc5910891e82e8f9d855d7
Jacqueline Nguyen <jnguyen9763@gmail.com>
2019-01-10 23:52:29 -0500

    Created canvas

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,2 +1,6 @@
-var petals = new Image();
-petals.src = "../images/petals.png";
+//var petals = new Image();
+//petals.src = "../images/petals.png";
+
+var canvas = document.querySelector('canvas');
+//canvas.width = window.innerWidth;
+//canvas.height = window.innerHeight;
