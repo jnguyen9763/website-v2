aaa7271a7e4b70e3b1a7cafac9b4209780bb187f
Jacqueline Nguyen <jnguyen9763@gmail.com>
2019-01-12 02:48:38 -0500

    Added little animation

--- scripts/website.js
+++ scripts/website.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
-    $('#front').children('h1').delay(3000).slideDown('slow').siblings('a').delay(4000).fadeIn('slow');
+    $('#front').children('h1').delay(2000).slideDown('slow').siblings('a').delay(3000).fadeIn('slow');
     // source: w3schools
-    $("a").on('click', function(event) {
-        if (this.hash !== "") {
+    $('a').on('click', function(event) {
+        if (this.hash !== '') {
             event.preventDefault();
             var hash = this.hash;
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 800, function(){
+            }, 800, function() {
                 window.location.hash = hash;
             });
         }
+    });
+    $(window).scroll(function (event) {
+        var scroll = $(window).scrollTop();
+        var opacityNum = (100 - scroll) / 100;
+        $('#front a').css('opacity', opacityNum);
     });
 });
