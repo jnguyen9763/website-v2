11c4568adc70a026f8fb5dc95f85f6c5fd4436e7
Jacqueline Nguyen <jnguyen9763@gmail.com>
2019-01-12 01:05:18 -0500

    Added animations

---
+++ scripts/website.js
@@ -0,0 +1,7 @@
+//setTimeout(function() {
+//
+//}, 5000);
+
+$(document).ready(function() {
+    $('#front').children('h1').delay(3000).slideDown('slow').siblings('a').delay(4000).fadeIn('slow');
+});
