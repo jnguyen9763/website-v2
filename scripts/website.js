e849559eeadcfc76ba1b4608953f8d6a20cc075d
Jacqueline Nguyen <jnguyen9763@gmail.com>
2019-01-12 01:36:34 -0500

    Added smooth scrolling

--- scripts/website.js
+++ scripts/website.js
@@ -1,7 +1,15 @@
-//setTimeout(function() {
-//
-//}, 5000);
-
 $(document).ready(function() {
     $('#front').children('h1').delay(3000).slideDown('slow').siblings('a').delay(4000).fadeIn('slow');
+    // source: w3schools
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
 });
