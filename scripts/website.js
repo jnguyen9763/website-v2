12f97f97ccc6a5d3c62a1aefbe0174b9d4fc8adf
Jacqueline Nguyen <jnguyen9763@gmail.com>
2019-01-13 19:57:16 -0500

    Added some animations to about section

--- scripts/website.js
+++ scripts/website.js
@@ -1,5 +1,17 @@
 var prevScroll = 0;
 
+// source: Scott Dowding from StackOverflow
+function isScrolledIntoView(elem)
+{
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
 $(document).ready(function() {
     $('#front').children('h1').delay(2000).slideDown('slow').siblings('a').delay(3000).fadeIn('slow');
     // source: w3schools
@@ -33,5 +45,20 @@ $(document).ready(function() {
             }
         }
         prevScroll = scroll;
+        /*
+        if (isScrolledIntoView($('#propic'))) {
+            $('html, body').animate({
+                scrollTop: $('#info').offset().top
+            }, 800);
+        }
+        */
+        if (isScrolledIntoView($('#info'))) {
+            $('#info h2').fadeTo('slow', 1).siblings('div').delay(1000).fadeTo('slow', 1);
+            $('#skills').delay(2000).fadeTo('slow', 1);
+            /*
+            $('#info h2').fadeIn().siblings('div').delay(1000).fadeIn();
+            $('#skills').delay(2000).fadeIn();
+            */
+        }
     });
 });
